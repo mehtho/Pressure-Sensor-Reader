cba5cfc2cfe15985acd0eded0041487b2de26168
Matthew Ho <18333157+mehtho@users.noreply.github.com>
2023-06-05 13:44:33 +0800

    Update reader.py

--- reader.py
+++ reader.py
@@ -11,16 +11,22 @@ ser = serial.Serial(
 print("connected to: " + ser.portstr)
 
 with open(str(time.time()) + '.csv', 'w') as f:
-    count = 0
     writer = csv.writer(f, delimiter=',', lineterminator='\n')
     writer.writerow(['time', 'reading'])
-
+    last = round(time.time() * 1000)
     while True:
-        line = ser.readline()
-        if line:
-            towrite = [str(count), line.decode('utf-8')[:-2]]
-            writer.writerow(towrite)
+        mills = round(time.time() * 1000)
+        if mills > last + 9:
+            last = mills
+
+            val = ser.readline()
+            while '\\n' not in str(val):
+                time.sleep(.001)
+                temp = ser.readline()
+                if not not temp.decode():
+                    val = (val.decode() + temp.decode()).encode()
+            val = val.decode()[:-2]
+
+            towrite = [mills, val]
             print(towrite)
-            count += 1
-
-
+            writer.writerow(towrite)
