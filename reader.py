caf80dac2a9f7a696c0945d74e34e91cb37f377b
Matthew Ho <18333157+mehtho@users.noreply.github.com>
2023-06-15 14:12:59 +0800

    Reliability

--- reader.py
+++ reader.py
@@ -5,10 +5,11 @@ import time
 
 ser = serial.Serial(
     port=sys.argv[1],
-    baudrate=9600,
+    baudrate=19200,
     timeout=0)
 
 print("connected to: " + ser.portstr)
+start = round(time.time() * 1000)
 
 with open(str(time.time()) + '.csv', 'w') as f:
     writer = csv.writer(f, delimiter=',', lineterminator='\n')
@@ -16,17 +17,16 @@ with open(str(time.time()) + '.csv', 'w') as f:
     last = round(time.time() * 1000)
     while True:
         mills = round(time.time() * 1000)
-        if mills > last + 9:
-            last = mills
+        last = mills
 
-            val = ser.readline()
-            while '\\n' not in str(val):
-                time.sleep(.001)
-                temp = ser.readline()
-                if not not temp.decode():
-                    val = (val.decode() + temp.decode()).encode()
-            val = val.decode()[:-2]
+        val = ser.readline()
+        while '\\n' not in str(val):
+            temp = ser.readline()
+            if not not temp.decode():
+                val = (val.decode() + temp.decode()).encode()
+        val = val.decode()[:-2]
+        res = val.split(';')
+        towrite = [start + int(res[0]), res[1]]
+        print(towrite)
+        writer.writerow(towrite)
 
-            towrite = [mills, val]
-            print(towrite)
-            writer.writerow(towrite)
